6aca9f4dcd14d0d1a945be692bfe65b6a9ed8381
Nhlahla Mike <nhlahlamikemabasa@gmail.com>
2019-10-22 16:43:23 +0200

    Created Edit function replacing Details and NewViewModel to ContactFormView

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -26,13 +26,40 @@ namespace Vidly.Controllers
 		{
 			// var membershipTypes = _context.MembershipTypes.ToList();
 			//var membershipTypes
-			var viewModel = new NewCustomerViewModel
+			var viewModel = new CustomerFormViewModel
 			{
 				// MembershipTypes = membershipTypes;
 			};
 			return View(viewModel);
 		}
-										   // GET: Customers
+
+		[HttpPost]
+		public ActionResult Create(Customer customer)
+		{
+			// _context.Customer.Add(customer);
+			// _context.SaveChanges();
+			return RedirectToAction("Index", "Customers");
+		}
+
+		public ActionResult Edit(int id)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id = id);
+
+			if (customer==null)
+			{
+				return HttpNotFound();
+			}
+
+			var viewModel = new CustomerFormViewModel
+			{
+				Customer = customer,
+				MembershipTypes = _context.MembershipTypes.ToList()
+			};
+
+			return View("New", viewModel);
+		}
+
+		// GET: Customers
 		public ActionResult Index()
 		{
 			var customers = GetCustomers();
